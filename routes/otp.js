93bb9d09f1e0ddc2eaa622e559e550afd9d56dbc
Priyadharshini-ma <priyanu2419@gmail.com>
2026-01-30 19:58:35 +0530

    fix render post issuess

--- routes/otp.js
+++ routes/otp.js
@@ -31,6 +31,10 @@ console.log("EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
       },
     });
 
+    await transporter.verify();
+console.log("SMTP server is ready");
+
+
     await transporter.sendMail({
       from: `"OTP Verification" <${process.env.EMAIL_USER}>`,
       to: email,
@@ -40,8 +44,10 @@ console.log("EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
 
     res.json({ message: "OTP sent successfully" });
   } catch (err) {
-    res.status(500).json({ message: "OTP send failed" });
-  }
+  console.error("OTP MAIL ERROR:", err);
+  res.status(500).json({ message: "OTP send failed" });
+}
+
 });
 
 router.post("/verify-otp", (req, res) => {
